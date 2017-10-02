72297797363953b19caa95cf38330e9177742e2d
marbisman <maria.arbisman@gmail.com>
2017-10-02 16:33:08 -0700

    updated letter-count

--- challenges/letter-count.js
+++ challenges/letter-count.js
@@ -12,7 +12,24 @@
     "e": 1
   }
   ```
+*/
 
+
+function letterCount(word){
+  var letters = new Object;
+  for (i=0; i < word.length; i++){
+    var l = word.charAt(i);
+    letters[l] = (isNan(letters[l]) ? 1: letters [l] +1);
+  }
+  //NaN represents not a number - when you're trying to count something that is not a number.
+  //? operator takes three operands and looks at the condition. condition ? experience 1 : experience 2
+  for (key in letters){
+    console.log (key + ' : ' + letters[key]);
+  }
+}
+
+letterCount("hellothere");
+/*
   Bonuses
   - Make sure that lower case letters and upper case letters count for the same character.
   - Ignore spaces, special characters, and punctuation.
